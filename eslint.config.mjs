c5cfa752d97412d5539e6bc8a1537683f8d3c785
Joao Guilherme dos Santos <joaoguilheremsantos2909@gmail.com>
2024-10-30 01:03:43 -0400

    Installed eslint and prittier

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      quotes: ['error', 'single'],
+      'no-console': 'warn',
+      eqeqeq: ['error', 'always'],
+      indent: ['error', 2],
+      'no-unused-vars': 'warn',
+    },
+  },
+];
